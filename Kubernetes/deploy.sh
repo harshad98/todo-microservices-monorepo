0c84ca54dcb3298a9cd8f1297340ddb5dc59e55b
Harshad198 <hhharshadraut98@gmail.com>
2026-06-29 19:06:56 +0530

    added k8s manifests and updated localhost of api gateway and frontend

---
+++ Kubernetes/deploy.sh
@@ -0,0 +1,13 @@
+kubectl apply -f namespace.yaml
+
+kubectl apply -f config/
+
+kubectl apply -f auth-service/
+
+kubectl apply -f todo-service/
+
+kubectl apply -f api-gateway/
+
+kubectl apply -f react-client/
+
+kubectl apply -f ingress/
